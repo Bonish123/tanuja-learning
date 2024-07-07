a322422cd91b993afcabb1c433cc7fe6f3e106a6
Bonish-Nitor <bonish.agarwal@nitorfinfotech.com>
2024-07-07 20:17:57 +0530

    Initial Commit for flask application

---
+++ app.py
@@ -0,0 +1,20 @@
+import json
+from flask import Flask, request, jsonify, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/add-user", methods=["GET", "POST"])
+def add_user():
+    
+    if request.method == "POST":
+        username = request.json.get("username")
+        age = request.json.get("age")
+        husband_name = request.json.get("husband_name")
+        
+        full_name = f"{username} and {husband_name} is {age} years old"
+        
+        return jsonify({"message": full_name})
+    else:
+        return render_template('index.html')
+
