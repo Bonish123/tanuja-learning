3589dd162e2e5a0ba88b6041ce441260015d737c
Bonish123 <bonishgarg@gmail.com>
2024-07-07 20:50:44 +0530

    Added service route

--- app.py
+++ app.py
@@ -29,3 +29,7 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+
+@app.route("/services")
+def services():
+    return render_template('services.html')
